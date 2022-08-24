9eadded0eca4605a9130ff80ac9f36405564bdc6
Rhyker Wells <a.rhykerw@gmail.com>
2022-08-24 01:15:29 +0100

    feat(*): Adds low level logging of current values

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,13 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "none" is now active!');
+    let CCW = vscode.window.createOutputChannel("YAGPDB");
+    CCW.show();
+    CCW.appendLine('YAGPDB cc ext has loaded, information will be output here')
+    CCW.appendLine('Language configured is ' + vscode.workspace.getConfiguration('yagpdbcc').get('syntaxHighlighting')!);
 
-    let disposable = vscode.commands.registerCommand('yagpdbcc.helloWorld', () => {
-        vscode.window.showInformationMessage('Hello World from YAGPDB!');
+    let disposable = vscode.commands.registerCommand('yagpdbcc.var', () => {
+        console.log(vscode.workspace.getConfiguration('yagpdbcc').get('syntaxHighlighting'));
     });
     context.subscriptions.push(disposable);
 }
